bad8386af07cf33498eac08a39a5bb68351ebdb0
ZaragD <daniel.zaragoza.h@gmail.com>
2018-02-12 00:56:04 -0800

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	//Called every frame
+	//void Update()
+	//{	}
+	public Text countText;
+	public Text winText;
+	public GameObject but;
+	public float speed;
+	private Rigidbody rb;
+	private int count;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		but = GameObject.FindWithTag ("Back To Menu");
+		but.gameObject.SetActive (false);
+
+		SetCountText ();
+		winText.text = " ";
+
+
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown("escape"))
+			but.gameObject.SetActive (true);
+	}
+		
+	//called before any physics calculation
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal"); // X
+		float moveVertical = Input.GetAxis ("Vertical"); // Z
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+
+	//Makes the Pickups collectible
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			count++;
+		
+			SetCountText ();
+		
+		}
+		
+	}
+	void SetCountText()
+	{
+		countText.text = "Count: = " + count.ToString ();
+		if (count >= 13) 
+		{
+			but.gameObject.SetActive (true);
+			winText.text = "You Win!";
+		
+		}
+	}
+}
+//Destroy (other.gameObject);
+//if(other.gameObject.CompareTag("Player")) cpmpares tag with a string value
+	//gameObject.SetActive(false);  activates or deactivates an object	
---
+++ Assets/Scripts/SecondController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SecondController : MonoBehaviour {
+
+
+	public float speed;
+	public float gravity;
+	public float jumpHeight;
+	public LayerMask ground;
+	public Transform feet;
+	public GameObject wall;
+	public GameObject button;
+	public Text winText;
+
+
+	private Vector3 direction;
+	private Vector3 walkingVelocity;
+	private Vector3 fallingVelocity;
+	private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		gravity = 9.8f;
+		direction = Vector3.zero;
+		fallingVelocity = Vector3.zero;
+		controller = GetComponent<CharacterController> ();
+		button.gameObject.SetActive (false);
+		winText.text = " ";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		direction.x = Input.GetAxis ("Horizontal");
+		direction.z = Input.GetAxis ("Vertical");
+		direction = direction.normalized;
+		walkingVelocity = direction * speed;
+		controller.Move (walkingVelocity * Time.deltaTime);
+		if (direction != Vector3.zero) 
+			{
+				transform.forward = direction;
+				Debug.Log (direction);
+			}
+		bool isGrounded = Physics.CheckSphere (feet.position, 0.1f, ground, QueryTriggerInteraction.Ignore);
+		if (isGrounded) 
+			{
+				fallingVelocity.y = 0f;
+			} 
+		else 
+			{
+				fallingVelocity.y -= gravity * Time.deltaTime;
+			}
+		if (Input.GetButtonDown ("Jump") && isGrounded) 
+			{
+				fallingVelocity.y = Mathf.Sqrt (gravity * jumpHeight);
+			}
+			
+		controller.Move(fallingVelocity * Time.deltaTime);
+
+		if (Input.GetKeyDown("escape"))
+			button.gameObject.SetActive (true);
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			wall.gameObject.SetActive (false);
+		}
+		if (other.gameObject.CompareTag ("Finish")) 
+		{
+			other.gameObject.SetActive (false);
+			winText.text = "You Win!";
+			button.gameObject.SetActive (true);
+
+		}
+	}
+}
